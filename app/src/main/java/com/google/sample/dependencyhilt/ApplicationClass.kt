c31295af01d1c764e4c83e4e7afedf747d6012da
fahadammar <fahadammar96@outlook.com>
2022-07-01 17:53:04 +0500

    The filed injection in the MainActivity or to say in the activity

--- app/src/main/java/com/google/sample/dependencyhilt/ApplicationClass.kt
+++ app/src/main/java/com/google/sample/dependencyhilt/ApplicationClass.kt
@@ -8,16 +8,10 @@ import javax.inject.Inject
 // We Need to use this notation for the application class - This Annotation behind the scenes generate the Dagger code
 @HiltAndroidApp
 class ApplicationClass : Application() {
-
-    // Fields cannot be private!!
-    @Inject
-    lateinit var fieldInjectionClass: FieldInjectionClass
-
     override fun onCreate() {
         super.onCreate()
 
-        // Field Injection
-        fieldInjectionClass.justLog()
+
     }
 }
 
--- app/src/main/java/com/google/sample/dependencyhilt/MainActivity.kt
+++ app/src/main/java/com/google/sample/dependencyhilt/MainActivity.kt
@@ -2,10 +2,31 @@ package com.google.sample.dependencyhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.sample.dependencyhilt.fieldInjection.FieldInjectionClass
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+// this Annotation is used for the classes, fragments etc
+// (search further we shall to put this annotation)
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    // FIELD INJECTION
+    @Inject
+    lateinit var fieldInjectionClass: FieldInjectionClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fieldInjectionClass.justLog()
     }
 }
+
+
+/*
+*
+* Adds a DI container to the Android
+* class annotated with it.
+* This requires using Hilt’s Gradle Plugin.
+*
+* */
